9eea88cbf9130e3686a393df26a3582e63786f47
Yury-Ryan <yuryryanmorais@gmail.com>
2025-11-30 15:43:06 -0300

    Add files via upload

--- script2.js
+++ script2.js
@@ -3,31 +3,31 @@ const tutorial = document.getElementById("tutorial");
 
 const tutoriais = {
   foto: `
-      <h2>📷 Mandar foto no WhatsApp</h2>
+      <h2>📷 Mandar foto no Whatsapp</h2>
       <ul>
-        <li>Abrir conversa <img src="ensino/whatsapp/passo.jpg"/ alt=""></li>
-        <li>Tocar no clipe <img src="ensino/whatsapp/passo2v3.jpg"/ alt=""></li>
-        <li>Escolher "Galeria" <img src="ensino/whatsapp/passo3v2.jpg"/ alt=""></li>
-        <li>Enviar <img src="ensino/whatsapp/passo4.jpg"/ alt=""></li>
+        <li>Abrir conversa <img src="ensino/Whatsapp/passo.jpg"/ alt=""></li>
+        <li>Tocar no clipe <img src="ensino/Whatsapp/passo2v3.jpg"/ alt=""></li>
+        <li>Escolher "Galeria" <img src="ensino/Whatsapp/passo3v2.jpg"/ alt=""></li>
+        <li>Enviar <img src="ensino/Whatsapp/passo4.jpg"/ alt=""></li>
       </ul>
       <button onclick="voltar()">🔙 Voltar</button>
     `,
   videochamada: `
       <h2>🎥 Fazer chamada de vídeo</h2>
       <ul>
-        <li>1. Abrir conversa<img src="ensino/whatsapp/passo.jpg"/ alt="">
+        <li>1. Abrir conversa<img src="ensino/Whatsapp/passo.jpg"/ alt="">
 </li>
-<li>2. Tocar na câmera <img src="ensino/whatsapp/passo2.jpg"/ alt=""></li>
-        <li>3. Aguardar a resposta <img src="ensino/whatsapp/passo3.png"/ alt=""></li>
+<li>2. Tocar na câmera <img src="ensino/Whatsapp/passo2.jpg"/ alt=""></li>
+        <li>3. Aguardar a resposta <img src="ensino/Whatsapp/passo3.png"/ alt=""></li>
       </ul>
       <button onclick="voltar()">🔙 Voltar</button>
     `,
   voz: `
       <h2> 📞 Fazer ligação de voz</h2>
       <ul>
-        <li>Abrir conversa <img src="ensino/whatsapp/passo.jpg"/ alt=""></li>
-        <li>Clicar no telefone <img src="ensino/whatsapp/passo2v2.jpg"/ alt=""></li>
-        <li>Esperar a ligação  <img src="ensino/whatsapp/passo3.png"/ alt=""></li>
+        <li>Abrir conversa <img src="ensino/Whatsapp/passo.jpg"/ alt=""></li>
+        <li>Clicar no telefone <img src="ensino/Whatsapp/passo2v2.jpg"/ alt=""></li>
+        <li>Esperar a ligação  <img src="ensino/Whatsapp/passo3.png"/ alt=""></li>
       </ul>
       <button onclick="voltar()">🔙 Voltar</button>
     `,
